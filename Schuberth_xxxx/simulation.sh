b396146047c9c309715860829fd6d67253925f3a
Manuel Rademaker <manuel.rademaker@uni-wuerzburg.de>
2020-02-25 17:51:16 +0100

    Test simulation 2 runs

--- Schuberth_xxxx/simulation.sh
+++ Schuberth_xxxx/simulation.sh
@@ -1,8 +1,20 @@
 #!bin/bash
 
-sudo Rscript 1_Simulation.R
+# Create file and save start time 
+sudo touch Timing.txt
+START=$(date)
 
+# Run R script 
+sudo Rscript 1_Simulation.R
+END=$(date)
+
+# Save start and end time in file
+
+sudo echo "The start time was: ${START}" >> Timing.txt
+sudo echo "The end time was: ${END}" >> Timing.txt
+
+# If succesfull do git pull and push
 git pull
 git add .
-git commit -m "Test simulation"
+git commit -m "Test simulation 2 runs"
 git push 
