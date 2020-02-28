eac01a0d13b8027f5d5179d2e269c9599e4aeebf
Manuel Rademaker <manuel.rademaker@uni-wuerzburg.de>
2020-02-28 08:06:08 +0100

    Simulation results QWF

--- Schuberth_xxxx/1_Simulation.R
+++ Schuberth_xxxx/1_Simulation.R
@@ -243,4 +243,4 @@ closeAllConnections() # close connection to relase RAM
 # Save objects =================================================================
 
 save(list = c("sim", "sample_size", "number_of_draws"),
-     file = "sim_hpc_200runs1.RData")
+     file = "sim_hpc_200runs_QWF.RData")
